991e6c7171318c210aaf579fdbeb221e0fb60e79
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2018-07-14 16:49:17 +0300

    Add index stlr js file with all modules exported

---
+++ es6/index.js
@@ -0,0 +1,21 @@
+import BlueimpGalleries    from './modules/BlueimpGalleries';
+import Entity              from './modules/ThreeJs/Entity';
+import FlashItems          from './modules/FlashItems';
+import Flips               from './modules/Flips';
+import html                from './modules/html';
+import PageEvent           from './modules/PageEvent';
+import Remodals            from './modules/Remodals';
+import SmoothScrollLinks   from './modules/SmoothScrollLinks';
+import WebFontLoader       from './modules/WebFontLoader';
+
+export {
+  BlueimpGalleries,
+  Entity,
+  FlashItems,
+  Flips,
+  html,
+  PageEvent,
+  Remodals,
+  SmoothScrollLinks,
+  WebFontLoader,
+};

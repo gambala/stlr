d155d93088bfd0277e0924fb9874cd37c9a59002
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2018-08-17 11:08:10 +0300

    Move view context seo helper methods into helper itself

--- app/controllers/concerns/with_seo.rb
+++ app/controllers/concerns/with_seo.rb
@@ -1,188 +1,89 @@
 module WithSeo
   extend ActiveSupport::Concern
-  include ActionView::Helpers::TagHelper
 
   included do
     class_attribute :attr_description,
-                    :attr_image,
                     :attr_keywords,
                     :attr_modified_time,
                     :attr_og_author,
+                    :attr_page_image,
                     :attr_pagetype,
                     :attr_published_time,
-                    :attr_site_image,
                     :attr_robots,
+                    :attr_site_image,
                     :attr_title
     helper_method :description,
-                  :image,
                   :keywords,
                   :modified_time,
                   :og_author,
+                  :page_image,
                   :pagetype,
                   :published_time,
                   :robots,
+                  :site_image,
                   :title
-    helper_method :meta_for_article,
-                  :meta_for_description,
-                  :meta_for_image,
-                  :meta_for_keywords,
-                  :meta_for_pagetype,
-                  :meta_for_robots,
-                  :meta_for_site_name,
-                  :meta_for_title,
-                  :meta_for_url
-    helper_method :title_tag
 
-    # Setters
-
-    def description(value)
+    def description(value = nil)
+      return attr_description if value.blank?
       self.attr_description = value.to_s
     end
 
-    def image(value)
-      self.attr_image = value.to_s
-    end
-
-    def keywords(value)
+    def keywords(value = nil)
+      return attr_keywords if value.blank?
       self.attr_keywords = value.to_s
     end
 
-    def modified_time(value)
-      return if value.blank?
+    def modified_time(value = nil)
+      return attr_modified_time if value.blank?
       self.attr_modified_time = value.iso8601
     end
 
-    def og_author(value)
+    def og_author(value = nil)
+      return attr_og_author if value.blank?
       self.attr_og_author = value
     end
 
-    def pagetype(value)
+    def page_image(value = nil)
+      return attr_page_image.presence || site_image if value.blank?
+      self.attr_page_image = value.to_s
+    end
+
+    def pagetype(value = nil)
+      return attr_pagetype if value.blank?
       self.attr_pagetype = value.to_s
     end
 
-    def published_time(value)
-      return if value.blank?
+    def published_time(value = nil)
+      return attr_published_time if value.blank?
       self.attr_published_time = value.iso8601
     end
 
-    def robots(value)
+    def robots(value = nil)
+      return attr_robots if value.blank?
       self.attr_robots = value.to_s
     end
 
+    def site_image(value = nil)
+      return attr_site_image if value.blank?
+      self.attr_site_image = value
+    end
+
     def title(value = nil, &block)
+      return attr_title if value.blank? && !block_given?
       self.attr_title = block_given? ? capture(&block) : value.to_s
-    end
-
-    # Getters with meta prefix
-
-    def meta_for_article
-      return if attr_pagetype.blank?
-      return if attr_pagetype != 'article'
-
-      if attr_published_time.present?
-        concat content_tag :meta, nil,
-                           content: attr_published_time,
-                           property: 'article:published_time'
-      end
-
-      if attr_modified_time.present?
-        concat content_tag :meta, nil,
-                           content: attr_modified_time,
-                           property: 'article:modified_time'
-      end
-
-      if attr_og_author.present?
-        content_tag :meta, nil,
-                    content: attr_og_author.join(', '),
-                    property: 'article:author'
-      end
-    end
-
-    def meta_for_description
-      return if attr_description.blank?
-      concat content_tag :meta, nil, content: attr_description, name: 'description'
-      content_tag :meta, nil, content: attr_description, property: 'og:description'
-    end
-
-    def meta_for_image
-      return if page_image.blank?
-      content_tag :meta, nil, content: page_image, property: 'og:image'
-    end
-
-    def meta_for_keywords
-      return if attr_keywords.blank?
-      content_tag :meta, nil, content: attr_keywords, name: 'keywords'
-    end
-
-    def meta_for_pagetype
-      return if attr_pagetype.blank?
-      content_tag :meta, nil, content: attr_pagetype, property: 'og:type'
-    end
-
-    def meta_for_robots
-      return if attr_robots.blank?
-      content_tag :meta, nil, content: attr_robots, name: 'robots'
-    end
-
-    def meta_for_site_name
-      content_tag :meta, nil, content: site_name, property: 'og:site_name'
-    end
-
-    def meta_for_title
-      return if attr_title.blank?
-      content_tag :meta, nil, content: attr_title, property: 'og:title'
-    end
-
-    def meta_for_url
-      content_tag :meta, nil, content: request.original_url, property: 'og:url'
-    end
-
-    def meta_html_prefix
-      return if attr_pagetype.blank?
-      return unless attr_pagetype.in? %w(article)
-      ['og: http://ogp.me/ns#',
-       "#{attr_pagetype}: http://ogp.me/ns/#{attr_pagetype}#"].join(' ')
-    end
-
-    # Getters with tag suffix
-
-    def title_tag
-      content_tag :title, attr_title.presence || site_name
-    end
-
-    # Other helpers
-
-    def text_from_html(value)
-      return nil if value.blank?
-      value = strip_tags(value)
-      value = truncate(value, length: 150)
-      value = value.gsub(/\n\n+/, '\n').gsub(/^\n|\n$/, ' ').squish
-      value
-    end
-
-    private
-
-    def page_image
-      attr_image.presence || site_image
-    end
-
-    def site_image
-      attr_site_image
-    end
-
-    def site_name
-      t('site.title')
     end
   end
 
   module ClassMethods
     # og_author %w(https://www.facebook.com/author_profile https://vk.com/author_profile)
-    def og_author(value)
+    def og_author(value = nil)
+      return attr_og_author if value.blank?
       self.attr_og_author = value
     end
 
     # site_image ActionController::Base.helpers.image_url('default-image.png')
-    def site_image(value)
+    def site_image(value = nil)
+      return attr_site_image if value.blank?
       self.attr_site_image = value
     end
   end
---
+++ app/helpers/seo_helper.rb
@@ -0,0 +1,88 @@
+module SeoHelper
+  def meta_for_article
+    return if pagetype.blank?
+    return if pagetype != 'article'
+
+    if published_time.present?
+      concat content_tag :meta, nil,
+                         content: published_time,
+                         property: 'article:published_time'
+    end
+
+    if modified_time.present?
+      concat content_tag :meta, nil,
+                         content: modified_time,
+                         property: 'article:modified_time'
+    end
+
+    if og_author.present?
+      content_tag :meta, nil,
+                  content: og_author.join(', '),
+                  property: 'article:author'
+    end
+  end
+
+  def meta_for_description
+    return if description.blank?
+    concat content_tag :meta, nil, content: description, name: 'description'
+    content_tag :meta, nil, content: description, property: 'og:description'
+  end
+
+  def meta_for_image
+    return if page_image.blank?
+    content_tag :meta, nil, content: page_image, property: 'og:image'
+  end
+
+  def meta_for_keywords
+    return if keywords.blank?
+    content_tag :meta, nil, content: keywords, name: 'keywords'
+  end
+
+  def meta_for_pagetype
+    return if pagetype.blank?
+    content_tag :meta, nil, content: pagetype, property: 'og:type'
+  end
+
+  def meta_for_robots
+    return if robots.blank?
+    content_tag :meta, nil, content: robots, name: 'robots'
+  end
+
+  def meta_for_site_name
+    content_tag :meta, nil, content: site_name, property: 'og:site_name'
+  end
+
+  def meta_for_title
+    return if title.blank?
+    content_tag :meta, nil, content: title, property: 'og:title'
+  end
+
+  def meta_for_url
+    content_tag :meta, nil, content: request.original_url, property: 'og:url'
+  end
+
+  def meta_html_prefix
+    return if pagetype.blank?
+    return unless pagetype.in? %w(article)
+    ['og: http://ogp.me/ns#',
+     "#{pagetype}: http://ogp.me/ns/#{pagetype}#"].join(' ')
+  end
+
+  def text_from_html(value)
+    return nil if value.blank?
+    value = strip_tags(value)
+    value = truncate(value, length: 150)
+    value = value.gsub(/\n\n+/, '\n').gsub(/^\n|\n$/, ' ').squish
+    value
+  end
+
+  def title_tag
+    content_tag :title, title.presence || site_name
+  end
+
+  private
+
+  def site_name
+    t('site.title')
+  end
+end

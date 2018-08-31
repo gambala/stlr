5db4d4cb83d05be0e98f4cfe41ea6b39c1a086af
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2018-08-31 10:36:56 +0300

    Make i18n fields model concern

--- app/models/concerns/with_i18n_fields.rb
+++ app/models/concerns/with_i18n_fields.rb
@@ -1,13 +1,11 @@
-module I18nFields
-  def i18n_fields(*args)
-    return @i18n_fields if args.empty?
+# frozen_string_literal: true
 
-    include ActionView::Helpers::TextHelper
-    include I18nHelper
+module WithI18nFields
+  extend ActiveSupport::Concern
+  include ActionView::Helpers::TextHelper
+  include I18nHelper
 
-    options = args.extract_options!
-    @i18n_fields = args
-
+  included do
     after_commit :create_translations, on: :create
 
     scope :translated, (lambda do |locale = I18n.locale|
@@ -15,7 +13,7 @@ module I18nFields
       where(id: ids)
     end)
 
-    define_method :create_translations do
+    def create_translations
       I18nLanguage.all.pluck(:locale).each do |locale|
         i18n_fields.each do |field|
           I18nTranslation.where(locale: locale, key: i18n_key(field)).first_or_create!
@@ -23,22 +21,22 @@ module I18nFields
       end
     end
 
-    define_method :i18n_fields do
+    def i18n_fields
       self.class.i18n_fields
     end
 
-    define_method :i18n_key do |field = nil|
+    def i18n_key(field = nil)
       result = "#{self.class.i18n_key_prefix}.id_#{id}"
       result += ".#{field}" if field.present?
       result
     end
 
-    define_method :translated? do |locale = I18n.locale|
+    def translated?(locale = I18n.locale)
       i18n_fields.each { |field| return false if public_send("i18n_#{field}", locale).blank? }
       true
     end
 
-    define_method :translations= do |value|
+    def translations=(value)
       return if value.blank?
 
       value.each do |locale, fields|
@@ -47,15 +45,24 @@ module I18nFields
         end
       end
     end
-
-    @i18n_fields.each do |field|
-      define_method("i18n_#{field}") do |locale = nil|
-        i18n_t i18n_key(field), locale: locale, record: self, field: field
-      end
-    end
   end
 
-  def i18n_key_prefix
-    "activerecord.values.#{model_name.i18n_key}"
+  class_methods do
+    def i18n_fields(*args)
+      return @i18n_fields if args.empty?
+
+      options = args.extract_options!
+      @i18n_fields = args
+
+      @i18n_fields.each do |field|
+        define_method("i18n_#{field}") do |locale = nil|
+          i18n_t i18n_key(field), locale: locale, record: self, field: field
+        end
+      end
+    end
+
+    def i18n_key_prefix
+      "activerecord.values.#{model_name.i18n_key}"
+    end
   end
 end
